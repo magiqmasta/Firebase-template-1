8023311f4a4ac9c00a09fbadc3b735d18fcc3a04
Fin <70215929+magiqmasta@users.noreply.github.com>
2024-08-28 11:34:19 +0100

    colour change

---
+++ app/components/HomeFeed.tsx
@@ -0,0 +1,10 @@
+import './HomeFeed.css';
+
+function HomeFeed() {
+  return (
+    <div>
+      <h1 className="home-feed-title">Home Feed</h1>
+      {/* ... rest of the component ... */}
+    </div>
+  );
+}
